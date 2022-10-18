6c24cdc01f2e43784f38c118ad247b6c57912820
SaadHadood <saad-96-08@hotmail.com>
2022-10-18 09:43:11 +0200

    before rep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './style.min.css';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
+import ContactsView from './views/ContactsView';
+import HomeView from './views/HomeView';
+import NotFoundView from './views/NotFoundView';
+import ShoppingCartView from './views/ShoppingCartView';
+import WishListView from './views/WishListView';
+import CompareView from './views/CompareView';
+import SearchView from './views/SearchView';
+import ProductsView from './views/ProductsView';
+import CategoriesView from './views/CategoriesView';
+import FooterSection from './sections/FooterSection';
+import MainMenuSection from './sections/MainMenuSection';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/categories" element={<CategoriesView />} />
+        <Route path="/products" element={<ProductsView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="/search" element={<SearchView />} />
+        <Route path="/compare" element={<CompareView />} />
+        <Route path="/wishlist" element={<WishListView />} />
+        <Route path="/shoppingcart" element={<ShoppingCartView />} />
+
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
