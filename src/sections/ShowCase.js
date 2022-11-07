92932f5f9cfec32504a742f56bbcf6a10d3b877e
SaadHadood <saad-96-08@hotmail.com>
2022-11-07 11:21:59 +0100

    nyVecka

--- src/sections/ShowCase.js
+++ src/sections/ShowCase.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import ShowCaseFemale from '../assets/images/showcase-female.svg'
-import ShowCaseMan from '../assets/images/showcase-man.svg'
 
 const ShowCase = () => {
   return (
   <section className="showcase">
       <div className="container">
-          <img src={ShowCaseFemale} alt="female sitting"/>
+          <img src="https://win22imgstorage.blob.core.windows.net/images/showcase-img-1.png" alt="female sitting" />
           <div className="showcase-body">
               <h1>SALE UP <br/> To 50% Off</h1>
               <p>Online shopping free home delivery over $100</p>
               <NavLink className="btn btn-lg btn-white" to="/shopnow" end>SHOP NOW</NavLink>
           </div>
-          <img src={ShowCaseMan} alt="model man"/>
+          <img src="https://win22imgstorage.blob.core.windows.net/images/showcase-img-2.png" alt="model man" />
       </div>
 </section>
 
